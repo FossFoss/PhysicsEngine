11cbc065162022df87d3564698a0f70300da2bc5
FossFoss <51982111+FossFoss@users.noreply.github.com>
2022-01-28 16:38:57 +1100

    Made global variables const

--- Project2D/GameDefines.h
+++ Project2D/GameDefines.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "glm/ext.hpp"
 
-glm::vec4 COLOUR_WHITE = { 1.0f, 1.0f, 1.0f, 1.0f };
-glm::vec4 COLOUR_RED = { 1.0f, 0.1f, 0.3f, 1.0f };
-glm::vec4 COLOUR_GREEN = { 0.2f, 1.0f, 0.2f, 1.0f };
-glm::vec4 COLOUR_BLUE = { 0.2f, 0.1f, 1.0f, 1.0f };
-glm::vec4 COLOUR_BROWN = { 0.8f, 0.6f, 0.1f, 1.0f };
+const glm::vec4 COLOUR_WHITE = { 1.0f, 1.0f, 1.0f, 1.0f };
+const glm::vec4 COLOUR_RED = { 1.0f, 0.1f, 0.3f, 1.0f };
+const glm::vec4 COLOUR_GREEN = { 0.2f, 1.0f, 0.2f, 1.0f };
+const glm::vec4 COLOUR_BLUE = { 0.2f, 0.1f, 1.0f, 1.0f };
+const glm::vec4 COLOUR_BROWN = { 0.8f, 0.6f, 0.1f, 1.0f };
 
-float MIN_LINEAR_THRESHOLD = 0.1f;
-float MIN_ANGULAR_THRESHOLD = 0.01f;
+const float MIN_LINEAR_THRESHOLD = 0.1f;
+const float MIN_ANGULAR_THRESHOLD = 0.01f;
